8955475aa77eddbac38fab51aba3c179507e1387
lukapys100 <l.pyszny1902@hotmail.com>
2021-01-31 20:55:12 +0100

    Commit for test

---
+++ 1_inner_abstract_interfaces/test/src/com/practise/javatraining/Main.java
@@ -0,0 +1,9 @@
+package com.practise.javatraining;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Test line of code.");
+        System.out.println("And another one.");
+    }
+}
