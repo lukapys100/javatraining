cc7cd538d170dbfa2a5e2ac8858c9da5db377448
lukapys100 <l.pyszny1902@hotmail.com>
2021-01-31 20:59:57 +0100

    The second test commit

--- 1_inner_abstract_interfaces/test/src/com/practise/javatraining/Main.java
+++ 1_inner_abstract_interfaces/test/src/com/practise/javatraining/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Test line of code.");
         System.out.println("And another one.");
+        System.out.println("One more");
     }
 }
