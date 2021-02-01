1d8902aadceef425db5b33a49ecfa216143c1e42
lukapys100 <l.pyszny1902@hotmail.com>
2021-02-01 11:44:17 +0100

    Delete all unnecessary test projects

--- 1_inner_abstract_interfaces/1/src/com/practise/javatraining/Main.java
+++
@@ -1,8 +0,0 @@
-package com.practise.javatraining;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("We will code the first project here.");
-    }
-}
